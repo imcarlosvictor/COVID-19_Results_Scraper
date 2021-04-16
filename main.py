a83c587c093592437779c05a121eff4cd01bfc29
Carlos Victor Torres <carlosvictortorres96@gmail.com>
2021-04-16 00:25:09 -0400

    Initial commit with OOP

--- main.py
+++ main.py
@@ -2,75 +2,98 @@ from bs4 import BeautifulSoup
 import requests
 
 
+class Data:
+
+    def __init__(self):
+        self.URL = 'https://www.worldometers.info/coronavirus/'
+        self.source = requests.get(self.URL)
+
+        self.soup = BeautifulSoup(self.source.content, 'html.parser')
+        self.case_results_block = self.soup.find_all('div', class_='col-md-6')
+
+    def http_response(self):
+        response = self.source.status_code
+        print(f'HTTP Response [{response}]')
+
+    def total_cases(self):
+
+        # Total Coronavirus Cases
+        total_cases = self.soup.find_all('h1')
+        total_cases_results = self.soup.find_all(
+            'div', class_='maincounter-number')
+
+        total_covid_cases = zip(total_cases, total_cases_results)
+
+        for x, y in total_covid_cases:
+            print(x.get_text(), y.get_text())
+
+    def active_cases(self):
+        # Active cases
+        active_cases_title = self.case_results_block[0].find(
+            'span', class_='panel-title').get_text()
+
+        active_infected_title = self.case_results_block[0].find(
+            'div', attrs={'style': 'font-size:13.5px'}).get_text()
+
+        active_infected_result = self.case_results_block[0].find(
+            class_='number-table-main').get_text()
+
+        mild_condition = self.case_results_block[0].find(
+            'div', attrs={'style': 'float:left; text-align:center'}).get_text()
+
+        critical_condition = self.case_results_block[0].find(
+            'div', attrs={'style': 'float:right; text-align:center'}).get_text()
+
+        results = {
+            'active_cases_title': active_cases_title,
+            'active_infected_title': active_infected_title,
+            'mild_condition': mild_condition,
+            'critical_condition': critical_condition
+        }
+
+        return results
+
+    def closed_cases(self):
+        # Closed Cases
+        closed_cases_title = self.case_results_block[1].find(
+            'span', class_='panel-title').get_text()
+
+        total_outcome_title = self.case_results_block[1].find(
+            'div', attrs={'style': 'font-size:13.5px'}).get_text()
+
+        total_outcome_result = self.case_results_block[1].find(
+            'div', class_='number-table-main').get_text()
+
+        recovered = self.case_results_block[1].find(
+            'div', attrs={'style': 'float:left; text-align:center'}).get_text()
+
+        deaths = self.case_results_block[1].find(
+            'div', attrs={'style': 'float:right; text-align:center'}).get_text()
+        pass
+
+    def table_data(self):
+        # Table Reports by Country/Territory
+        # table = soup.find('div', class_='row')
+        # table_title = soup.find('h3', id='countries').get_text()
+        # table_results = table_contents.find('tr')
+        table_contents = self.soup.find(
+            'table', id='main_table_countries_today')
+        table_body = table_contents.find('tbody')
+
+        # print(table_contents)
+        print(table_body.get_text())
+        # print(table_body.get_text())
+        pass
+
+
 def main():
 
-    BASEURL = 'https://www.worldometers.info/coronavirus/'
-    source = requests.get(BASEURL)
+    covid_data = Data()
 
-    response = source.status_code
-    print(f'HTTP Response [{response}]')
-
-    soup = BeautifulSoup(source.text, 'lxml')
-
-    # Total Coronavirus Cases
-    total_cases = soup.find_all('h1')
-    total_cases_results = soup.find_all('div', class_='maincounter-number')
-
-    total_covid_cases = zip(total_cases, total_cases_results)
-
-    for x, y in total_covid_cases:
-        print(x.get_text(), y.get_text())
-
-    case_results_block = soup.find_all('div', class_='col-md-6')
-
-    # Active cases
-    active_cases_title = case_results_block[0].find(
-        'span', class_='panel-title').get_text()
-
-    active_infected_title = case_results_block[0].find(
-        'div', attrs={'style': 'font-size:13.5px'}).get_text()
-
-    active_infected_result = case_results_block[0].find(
-        class_='number-table-main').get_text()
-
-    mild_condition = case_results_block[0].find(
-        'div', attrs={'style': 'float:left; text-align:center'}).get_text()
-
-    critical_condition = case_results_block[0].find(
-        'div', attrs={'style': 'float:right; text-align:center'}).get_text()
+    for title, data in covid_data.active_cases().items():
+        print(title)
 
 
-    # # Closed Cases
-    closed_cases_title = case_results_block[1].find(
-        'span', class_='panel-title').get_text()
-
-    total_outcome_title = case_results_block[1].find(
-        'div', attrs={'style': 'font-size:13.5px'}).get_text()
-
-    total_outcome_result = case_results_block[1].find(
-        'div', class_='number-table-main').get_text()
-
-    recovered = case_results_block[1].find(
-        'div', attrs={'style': 'float:left; text-align:center'}).get_text()
-
-    deaths = case_results_block[1].find(
-        'div', attrs={'style': 'float:right; text-align:center'}).get_text()
-
-
-    # Table Reports by Country/Territory
-
-    # table = soup.find('div', class_='row')
-    # table_title = soup.find('h3', id='countries').get_text()
-    table_contents = soup.find('table', id='main_table_countries_today')
-    # table_results = table_contents.find('tr')
-    table_body = table_contents.find('tbody')
-
-    # print(table_contents)
-    # print(table_body.get_text())
-
-    
-
-    
 
 if __name__ == '__main__':
     main()
